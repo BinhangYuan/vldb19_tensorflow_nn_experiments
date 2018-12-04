abdc087842b506729683c15e14206dd7bf79133b
yuanbinhang@hotmail.com <yuanbinhang@hotmail.com>
2018-12-03 21:59:35 -0600

    a different partitioner

--- dnn_train_aws_sync_cpu.py
+++ dnn_train_aws_sync_cpu.py
@@ -120,9 +120,15 @@ elif FLAGS.job_name == "worker":
         # model parameters will change during training so we use tf.Variable
         tf.set_random_seed(1)
 
-        W1 = tf.get_variable("W1", [D1, D2], initializer=tf.random_normal_initializer(), partitioner=tf.min_max_variable_partitioner(max_partitions=10*num_ps_replicas))
-        W2 = tf.get_variable("W2", [D2, D3], initializer=tf.random_normal_initializer(), partitioner=tf.min_max_variable_partitioner(max_partitions=10*num_ps_replicas))
-        W3 = tf.get_variable("W3", [D3, C], initializer=tf.random_normal_initializer(), partitioner=tf.min_max_variable_partitioner(max_partitions=10*num_ps_replicas))
+        #W1 = tf.get_variable("W1", [D1, D2], initializer=tf.random_normal_initializer(), partitioner=tf.min_max_variable_partitioner(max_partitions=10*num_ps_replicas))
+        #W2 = tf.get_variable("W2", [D2, D3], initializer=tf.random_normal_initializer(), partitioner=tf.min_max_variable_partitioner(max_partitions=10*num_ps_replicas))
+        #W3 = tf.get_variable("W3", [D3, C], initializer=tf.random_normal_initializer(), partitioner=tf.min_max_variable_partitioner(max_partitions=10*num_ps_replicas))
+        W1 = tf.get_variable("W1", [D1, D2], initializer=tf.random_normal_initializer(),
+                             partitioner=tf.fixed_size_partitioner(num_shards= num_ps_replicas))
+        W2 = tf.get_variable("W2", [D2, D3], initializer=tf.random_normal_initializer(),
+                             partitioner=tf.fixed_size_partitioner(num_shards=num_ps_replicas))
+        W3 = tf.get_variable("W3", [D3, C], initializer=tf.random_normal_initializer(),
+                             partitioner=tf.fixed_size_partitioner(num_shards=num_ps_replicas))
         b1 = tf.Variable(tf.zeros([D2]))
         b2 = tf.Variable(tf.zeros([D3]))
         b3 = tf.Variable(tf.zeros([C]))
